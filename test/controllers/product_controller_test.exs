ab8d69d9ca9c5e8fd4ba9ecc46c89ec77fb8c664
Luiz Varela <luiz.lvarela@gmail.com>
2016-01-16 12:42:49 -0300

    Scope products API with market resources and improve all specs related to products controller

--- test/controllers/product_controller_test.exs
+++ test/controllers/product_controller_test.exs
@@ -2,63 +2,86 @@ defmodule MarketApi.ProductControllerTest do
   use MarketApi.ConnCase
 
   alias MarketApi.Product
+  alias MarketApi.Market
+
   @valid_attrs %{barcode: "some content", image: "some content", name: "some content", price: "120.5"}
-  @invalid_attrs %{}
+  @invalid_attrs %{name: ""}
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    {:ok, market} = create_market
+
+    {:ok, conn: conn |> put_req_header("accept", "application/vnd.api+json") |> put_req_header("content-type", "application/vnd.api+json"), market: market}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, product_path(conn, :index)
+  test "list all entries on index", %{conn: conn, market: market} do
+    conn = get conn, market_product_path(conn, :index, market)
     assert json_response(conn, 200)["data"] == []
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    product = Repo.insert! %Product{}
-    conn = get conn, product_path(conn, :show, product)
-    assert json_response(conn, 200)["data"] == %{"id" => product.id,
+  test "shows chosen resource", %{conn: conn, market: market} do
+    product = build_product market
+    conn = get conn, market_product_path(conn, :show, market, product)
+    assert json_response(conn, 200)["data"] == %{ "attributes" => %{
+      "id" => product.id,
       "name" => product.name,
       "barcode" => product.barcode,
       "image" => product.image,
-      "price" => product.price,
-      "market_id" => product.market_id}
+      "price" => Decimal.to_string(product.price),
+      "market-id" => product.market_id},
+      "id" => Integer.to_string(product.id), "type" => "product"}
   end
 
-  test "does not show resource and instead throw error when id is nonexistent", %{conn: conn} do
+  test "does not show resource and instead throw error when id is nonexistent", %{conn: conn, market: market} do
     assert_error_sent 404, fn ->
-      get conn, product_path(conn, :show, -1)
+      get conn, market_product_path(conn, :show, market, -1)
     end
   end
 
-  test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, product_path(conn, :create), product: @valid_attrs
+  test "creates and renders resource when data is valid", %{conn: conn, market: market} do
+    conn = conn()
+      |> put_req_header("accept", "application/vnd.api+json")
+      |> put_req_header("content-type", "application/vnd.api+json")
+      |> post market_product_path(conn, :create, market), product: @valid_attrs
+
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Product, @valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, product_path(conn, :create), product: @invalid_attrs
+  test "does not create resource and renders errors when data is invalid", %{conn: conn, market: market} do
+    conn = post conn, market_product_path(conn, :create, market), product: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    product = Repo.insert! %Product{}
-    conn = put conn, product_path(conn, :update, product), product: @valid_attrs
+  test "updates and renders chosen resource when data is valid", %{conn: conn, market: market} do
+    product = build_product market
+    conn = put conn, market_product_path(conn, :update, market, product), product: @valid_attrs
     assert json_response(conn, 200)["data"]["id"]
     assert Repo.get_by(Product, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    product = Repo.insert! %Product{}
-    conn = put conn, product_path(conn, :update, product), product: @invalid_attrs
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, market: market} do
+    product = build_product market
+    conn = put conn, market_product_path(conn, :update, market, product), product: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "deletes chosen resource", %{conn: conn} do
-    product = Repo.insert! %Product{}
-    conn = delete conn, product_path(conn, :delete, product)
+  test "deletes chosen resource", %{conn: conn, market: market} do
+    product = build_product market
+    conn = delete conn, market_product_path(conn, :delete, market, product)
     assert response(conn, 204)
     refute Repo.get(Product, product.id)
+  end
+
+  defp create_market do
+    Market.changeset(%Market{}, %{name: "Market Foo", phone: 123})
+    |> Repo.insert
+  end
+
+  defp build_product(market) do
+    changeset =
+      market
+      |> build(:products)
+      |> Product.changeset(@valid_attrs)
+    Repo.insert!(changeset)
   end
 end
